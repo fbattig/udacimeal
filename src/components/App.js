62312a2bb7f43dbec86379fb4431f8dd2aa4f58d
Felix Battig <fbattig@yahoo.com>
2019-01-05 17:36:18 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -73,7 +73,7 @@ class App extends Component {
       <div className='container'>
 
         <div className='nav'>
-          <h1 className='header'>UdaciMeals</h1>
+          <h1 className='header'>Weekly meals</h1>
           <button
             className='shopping-list'
             onClick={this.openIngredientsModal}>
